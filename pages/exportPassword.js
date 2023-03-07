b2db8c1bafcc74442c31813513af7836bdd0bc5b
kaligraphy247 <chikeziejames3@gmail.com>
2023-03-07 18:39:15 +0100

    gearing up for importing and exporting function

--- pages/exportPassword.js
+++ pages/exportPassword.js
@@ -1,5 +1,18 @@
 import Head from "next/head";
 import QuickLinks from "../components/quickLinks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFileImport,
+  faCircleInfo,
+  faFileExport,
+  faDownload,
+} from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
+const fileImport = <FontAwesomeIcon icon={faFileImport} />;
+const info = <FontAwesomeIcon icon={faCircleInfo} />;
+const fileExport = <FontAwesomeIcon icon={faFileExport} />;
+const download = <FontAwesomeIcon icon={faDownload} />;
 
 export default function ExportPasswords() {
   return (
@@ -9,7 +22,36 @@ export default function ExportPasswords() {
       </Head>
       <h1 className="text-3xl font-bold text-center mb-2">Export Passwords</h1>
       <h3 className="text-2xl font-bold py-1">Export your passwords here</h3>
-      <div className="container">1</div>
+      <div className="container">
+        <h2 className="font-medium text-lg">
+          Export your passwords to a valid JSON file that can be downloaded.
+        </h2>
+
+        <form className="border-2 py-2 px-1 border-dotted">
+          <div className="pb-3 text-sm">
+            <p>
+              <span className="text-blue-600">{info}</span> Suitable for files
+              to be used for{" "}
+              <Link
+                href="/importPassword"
+                className="hover:text-red-500 text-blue-700"
+              >
+                Import {fileImport}
+              </Link>
+            </p>
+          </div>
+          <input
+            type="file"
+            className="text-blue-500 text-sm file:text-blue-500 file:rounded file:px-2 file:py-1 file:border-none file:bg-blue-100 file:text-sm file:hover:bg-blue-200 file:hover:text-blue-600 focus:outline-blue-400"
+          />
+          <div className="text-center">
+            <button className="border px-2 py-1 rounded font-semibold shadow-md">
+              Export {fileExport}
+            </button>
+          </div>
+        </form>
+        <div className="py-2 px-1 mt-6">Ready to download {download}</div>
+      </div>
       <QuickLinks />
     </>
   );
--- pages/importPassword.js
+++ pages/importPassword.js
@@ -1,5 +1,16 @@
 import Head from "next/head";
 import QuickLinks from "../components/quickLinks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFileImport,
+  faCircleInfo,
+  faFileExport,
+} from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
+const fileImport = <FontAwesomeIcon icon={faFileImport} />;
+const info = <FontAwesomeIcon icon={faCircleInfo} />;
+const fileExport = <FontAwesomeIcon icon={faFileExport} />;
 
 export default function ImportPasswords() {
   return (
@@ -8,8 +19,36 @@ export default function ImportPasswords() {
         <title>Import Password</title>
       </Head>
       <h1 className="text-3xl font-bold text-center mb-2">Import Passwords</h1>
-      <h3 className="text-2xl font-bold py-1">Import your passwords here</h3>
-      <div className="container">1</div>
+      <h3 className="text-2xl font-bold py-1">Import your password</h3>
+      <div className="container">
+        <h2 className="font-medium text-lg">
+          Import your passwords from a valid JSON file
+        </h2>
+
+        <form className="border-2 py-2 px-1 border-dotted">
+          <div className="pb-3 text-sm">
+            <p>
+              <span className="text-blue-600">{info}</span> Only works on
+              passwords exported from{" "}
+              <Link
+                href="/exportPassword"
+                className="hover:text-red-500 text-blue-700"
+              >
+                Export {fileExport}
+              </Link>
+            </p>
+          </div>
+          <input
+            type="file"
+            className="text-blue-500 text-sm file:text-blue-500 file:rounded file:px-2 file:py-1 file:border-none file:bg-blue-100 file:text-sm file:hover:bg-blue-200 file:hover:text-blue-600 focus:outline-blue-400"
+          />
+          <div className="text-center">
+            <button className="border px-2 py-1 rounded font-semibold shadow-md">
+              Import {fileImport}
+            </button>
+          </div>
+        </form>
+      </div>
       <QuickLinks />
     </>
   );
